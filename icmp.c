359275d07427fab88143ad1019e44978f25b4d37
tsukipond8531 <reader2027@gmail.com>
2024-01-07 20:54:29 +0100

    add(icmp): ICMP control basic functions to pack / unpack and create packets

---
+++ icmp.c
@@ -0,0 +1,152 @@
+#include "icmp.h"
+#include "ip_header.h"
+#include "utils.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+t_icmp *create_icmp(t_icmp_types const type) {
+  t_icmp *icmp;
+
+  icmp = malloc(sizeof(t_icmp));
+  if (!icmp) {
+    printf("An error occurred whilst allocating memory\n");
+    exit(1);
+  }
+  icmp->code = 0;
+  icmp->checksum = 0;
+  icmp->payload.echo.identifier = 0;
+  icmp->payload.echo.sequence = 0;
+  for (size_t i = 0; i < DATA_SIZE; i++)
+    icmp->payload.echo.data[i] = 0;
+  icmp->type = type;
+  switch (icmp->type) {
+  case ICMP_ECHO:
+  case ICMP_ECHO_REPLY:
+    // This behaviour is undefined for ICMP_ECHO_REPLY as the ICMP_ECHO_REPLY
+    // should be provided of an identifier/seq matching the original ICMP_ECHO
+    icmp->payload.echo.identifier = 0x4242;
+    break;
+  default:
+    errno = EINVAL;
+    free(icmp);
+    return NULL;
+  }
+  set_icmp_checksum(icmp);
+  return icmp;
+}
+
+uint8_t *icmp_to_packet(t_ip_header const *const header,
+                        t_icmp const *const payload) {
+  uint8_t *bytes;
+  uint8_t *header_bytes;
+  uint8_t *payload_bytes;
+
+  if (header->type_of_service != 0)
+    return NULL;
+  if (header->total_length !=
+      WORDS_TO_BYTES(header->internet_header_length) + sizeof(t_icmp))
+    return NULL;
+  if (header->protocol != 1)
+    return NULL;
+  header_bytes = pack_ip_header(header);
+  payload_bytes = pack_icmp_payload(payload);
+  bytes = malloc(header->total_length);
+  if (!bytes) {
+    printf("An error occurred whilst allocating memory\n");
+    exit(1);
+  }
+  ft_memcpy(bytes, header_bytes,
+            WORDS_TO_BYTES(header->internet_header_length));
+  ft_memcpy(bytes + WORDS_TO_BYTES(header->internet_header_length),
+            payload_bytes, sizeof(*payload));
+  free(payload_bytes);
+  free(header_bytes);
+  return bytes;
+}
+
+uint8_t *pack_icmp_payload(t_icmp const *const payload) {
+  t_icmp copy;
+  void *bytes;
+
+  copy = *payload;
+  copy.checksum = htons(payload->checksum);
+  if (payload->type == ICMP_ECHO || payload->type == ICMP_ECHO_REPLY) {
+    copy.payload.echo.identifier = htons(payload->payload.echo.identifier);
+    copy.payload.echo.sequence = htons(payload->payload.echo.sequence);
+  }
+
+  bytes = malloc(sizeof(t_icmp));
+  if (!bytes) {
+    printf("An error occurred whilst allocating memory\n");
+    exit(1);
+  }
+  ft_memcpy(bytes, &copy, sizeof(t_icmp));
+  return bytes;
+}
+
+t_ip_header *unpack_icmp_header_from_packet(void const *const bytes) {
+  return unpack_ip_header(bytes);
+}
+
+t_icmp *unpack_icmp_payload_from_packet(void const *const bytes) {
+  t_ip_header *ip;
+  t_icmp *icmp;
+
+  ip = unpack_icmp_header_from_packet(bytes);
+  if (!is_valid_checksum(bytes + WORDS_TO_BYTES(ip->internet_header_length),
+                         ip->total_length -
+                             WORDS_TO_BYTES(ip->internet_header_length))) {
+    free(ip);
+    return NULL;
+  }
+  icmp = malloc(sizeof(t_icmp));
+  if (!icmp) {
+    printf("An error occurred whilst allocating memory\n");
+    exit(1);
+  }
+  ft_memcpy(icmp, bytes + WORDS_TO_BYTES(ip->internet_header_length),
+            ip->total_length - WORDS_TO_BYTES(ip->internet_header_length));
+  icmp->checksum = ntohs(icmp->checksum);
+  if (icmp->type == ICMP_ECHO || icmp->type == ICMP_ECHO_REPLY) {
+    icmp->payload.echo.identifier = ntohs(icmp->payload.echo.identifier);
+    icmp->payload.echo.sequence = ntohs(icmp->payload.echo.sequence);
+  }
+  free(ip);
+  return icmp;
+}
+
+void set_identifier(t_icmp *const packet, uint16_t identifier) {
+  if (packet->type == ICMP_ECHO || packet->type == ICMP_ECHO_REPLY)
+    packet->payload.echo.identifier = identifier;
+  set_icmp_checksum(packet);
+}
+
+void set_sequence(t_icmp *const packet, uint16_t const seq) {
+  if (packet->type == ICMP_ECHO || packet->type == ICMP_ECHO_REPLY)
+    packet->payload.echo.sequence = seq;
+  set_icmp_checksum(packet);
+}
+
+void set_sequence_more(t_icmp *const packet) {
+  if (packet->type == ICMP_ECHO || packet->type == ICMP_ECHO_REPLY)
+    packet->payload.echo.sequence++;
+  set_icmp_checksum(packet);
+}
+
+void set_data(t_icmp *const packet, uint8_t data[DATA_SIZE],
+              uint8_t data_size) {
+  if (packet->type == ICMP_ECHO || packet->type == ICMP_ECHO_REPLY)
+    ft_memcpy(packet->payload.echo.data, data,
+              data_size > DATA_SIZE ? DATA_SIZE : data_size);
+  set_icmp_checksum(packet);
+}
+
+void set_icmp_checksum(t_icmp *const packet) {
+  void *bytes;
+
+  packet->checksum = 0;
+  bytes = pack_icmp_payload(packet);
+  packet->checksum = compute_checksum(bytes, sizeof(t_icmp));
+  free(bytes);
+}
--- tests.c
+++ tests.c
@@ -1,7 +1,11 @@
+#include "icmp.h"
 #include "ip_header.h"
 #include "utils.h"
 #include <assert.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void utils_compute_checksum(void) {
   {
@@ -12,6 +16,7 @@ void utils_compute_checksum(void) {
 
     assert(checksum == 0xb861);
   }
+  printf("[OK][compute_checksum] Basic\n");
   {
     char bytes[20] = {0x45, 0x00, 0x00, 0x54, 0x20, 0xb8, 0x00,
                       0x00, 0x40, 0x01, 0x00, 0x00, 0xc0, 0xa8,
@@ -20,6 +25,7 @@ void utils_compute_checksum(void) {
 
     assert(checksum == 0xdb91);
   }
+  printf("[OK][compute_checksum] Basic 2\n");
   {
 
     char bytes[20] = {0x45, 0x00, 0x00, 0x54, 0x00, 0x00, 0x00,
@@ -29,6 +35,7 @@ void utils_compute_checksum(void) {
 
     assert(checksum == 0xca49);
   }
+  printf("[OK][compute_checksum] Basic 3\n");
   {
 
     char bytes[20] = {0x45, 0x00, 0x00, 0x54, 0x00, 0x00, 0x00,
@@ -38,6 +45,7 @@ void utils_compute_checksum(void) {
 
     assert(checksum != 0xca49);
   }
+  printf("[OK][compute_checksum] Inequality\n");
 }
 
 void utils_is_valid_checksum(void) {
@@ -48,6 +56,7 @@ void utils_is_valid_checksum(void) {
 
     assert(is_valid_checksum(bytes, 20));
   }
+  printf("[OK][is_valid_checksum] Basic\n");
   {
     char bytes[20] = {0x45, 0x00, 0x00, 0x54, 0x20, 0xb8, 0x00,
                       0x00, 0x40, 0x01, 0xdb, 0x91, 0xc0, 0xa8,
@@ -55,6 +64,7 @@ void utils_is_valid_checksum(void) {
 
     assert(is_valid_checksum(bytes, 20));
   }
+  printf("[OK][is_valid_checksum] Basic 2\n");
   {
 
     char bytes[20] = {0x45, 0x00, 0x00, 0x54, 0x00, 0x00, 0x00,
@@ -63,6 +73,7 @@ void utils_is_valid_checksum(void) {
 
     assert(is_valid_checksum(bytes, 20));
   }
+  printf("[OK][is_valid_checksum] Basic 3\n");
   {
 
     char bytes[20] = {0x45, 0x00, 0x00, 0x54, 0x00, 0x00, 0x00,
@@ -71,10 +82,10 @@ void utils_is_valid_checksum(void) {
 
     assert(!is_valid_checksum(bytes, 20));
   }
+  printf("[OK][is_valid_checksum] Inequality\n");
 }
 
 void utils_ft_memcpy(void) {
-  // Basic byte test
   {
     char bytes[16];
     char source[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
@@ -84,7 +95,7 @@ void utils_ft_memcpy(void) {
     for (size_t i = 0; i < 16; i++)
       assert(*(bytes + i) == *(source + i));
   }
-  // Different data type test
+  printf("[OK][ft_memcpy] Basic byte test\n");
   {
     int bytes[16];
     int source[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
@@ -96,15 +107,15 @@ void utils_ft_memcpy(void) {
     for (size_t i = 0; i < 16 * sizeof(int); i++)
       assert(*(cmpbytes + i) == *(cmpsource + i));
   }
+  printf("[OK][ft_memcpy] Different data type size test\n");
 }
 
 void ip_header(void) {
-  // Basic create and delete
   {
     t_ip_header *header = create_ip_header();
     free(header);
   }
-  // All attribute setter with checksum validation
+  printf("[OK][t_ip_header] Basic create and delete\n");
   {
     t_ip_header *header = create_ip_header();
     uint8_t *bytes;
@@ -126,7 +137,7 @@ void ip_header(void) {
     free(bytes);
     free(header);
   }
-  // Multiple repack to assess checksum and data safety
+  printf("[OK][t_ip_header] All attribute setter with checksum validation\n");
   {
     t_ip_header *a = create_ip_header();
 
@@ -149,6 +160,125 @@ void ip_header(void) {
     free(b);
     free(a);
   }
+  printf(
+      "[OK][t_ip_header] Multiple repack to assess checksum and data safety\n");
+}
+
+void icmp(void) {
+  uint8_t empty_data[32] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+                            0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+                            0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+                            0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+  uint8_t random_data[32] = {0x42, 0x42, 0x42, 0x42, 0x42,
+                             0x42, 0x42, 0x42, 0x42};
+
+  {
+    t_icmp *icmp = create_icmp(ICMP_ECHO);
+
+    assert(icmp->type == ICMP_ECHO);
+    assert(icmp->code == 0);
+    assert(icmp->payload.echo.sequence == 0);
+    assert(icmp->payload.echo.identifier == 0x4242);
+    assert(!memcmp(icmp->payload.echo.data, empty_data, 32));
+
+    free(icmp);
+  }
+  printf("[OK][t_icmp] Basic ICMP creation\n");
+  // Set basic sequence and identifier
+  {
+    t_icmp *icmp = create_icmp(ICMP_ECHO);
+
+    set_sequence(icmp, 19);
+    assert(icmp->payload.echo.sequence == 19);
+
+    set_sequence_more(icmp);
+    assert(icmp->payload.echo.sequence == 20);
+
+    set_identifier(icmp, 0x4242);
+    assert(icmp->payload.echo.identifier == 0x4242);
+    free(icmp);
+  }
+  printf("[OK][t_icmp] Set basic seq and identifier\n");
+  {
+    t_icmp *icmp = create_icmp(ICMP_ECHO);
+
+    set_identifier(icmp, 0x7c8d);
+    set_sequence(icmp, 0x1c00);
+    set_data(icmp, random_data, 9);
+    assert(icmp->checksum == 0x1469);
+
+    free(icmp);
+  }
+  printf("[OK][t_icmp] Checksum test\n");
+  {
+    t_icmp *icmp = create_icmp(219);
+    assert(errno == EINVAL);
+    assert(icmp == NULL);
+  }
+  printf("[OK][t_icmp] Create non-registered ICMP kind\n");
+  {
+    t_icmp *icmp = create_icmp(ICMP_ECHO);
+    uint8_t *bytes;
+    uint8_t data[32] = {0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+                        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+                        0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+                        0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27};
+
+    set_identifier(icmp, 0x7ceb);
+    set_sequence(icmp, 0x0400);
+    set_data(icmp, data, 32);
+
+    bytes = pack_icmp_payload(icmp);
+    assert(is_valid_checksum(bytes, sizeof(t_icmp)));
+
+    free(bytes);
+    free(icmp);
+  }
+  printf("[OK][t_icmp] Checksum validation after packing\n");
+  {
+    t_ip_header *header = create_ip_header();
+    t_icmp *icmp = create_icmp(ICMP_ECHO);
+
+    set_body_length(header, sizeof(t_icmp));
+    set_protocol(header, IP_ICMP);
+    set_time_to_live(header, 64);
+
+    set_identifier(icmp, 0x1942);
+    set_sequence(icmp, 0x199);
+
+    uint8_t *p1 = icmp_to_packet(header, icmp);
+    assert(p1 != NULL);
+    t_ip_header *hu1 = unpack_icmp_header_from_packet(p1);
+    t_icmp *u1 = unpack_icmp_payload_from_packet(p1);
+    uint8_t *p2 = icmp_to_packet(hu1, u1);
+    assert(p2 != NULL);
+    t_ip_header *hu2 = unpack_icmp_header_from_packet(p2);
+    t_icmp *u2 = unpack_icmp_payload_from_packet(p2);
+    uint8_t *p3 = icmp_to_packet(hu2, u2);
+    assert(p3 != NULL);
+    t_ip_header *hu3 = unpack_icmp_header_from_packet(p3);
+    t_icmp *u3 = unpack_icmp_payload_from_packet(p3);
+
+    assert(hu3->protocol == IP_ICMP);
+    assert(hu3->total_length ==
+           WORDS_TO_BYTES(hu3->internet_header_length) + sizeof(t_icmp));
+    assert(hu3->time_to_live == 64);
+    assert(u3->payload.echo.identifier == 0x1942);
+    assert(u3->payload.echo.sequence == 0x199);
+    free(hu3);
+    free(u3);
+    free(p3);
+    free(hu2);
+    free(u2);
+    free(p2);
+    free(hu1);
+    free(u1);
+    free(p1);
+    free(icmp);
+    free(header);
+  }
+  printf("[OK][t_icmp] Created - pack - unpack - pack - unpack test IP header "
+         "and ICMP payload\n");
 }
 
 int main(void) {
@@ -156,4 +286,5 @@ int main(void) {
   utils_is_valid_checksum();
   utils_ft_memcpy();
   ip_header();
+  icmp();
 }
