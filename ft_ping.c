6be14d9c189096d52eb440c2cf6cf97cfe25a3a1
tsukipond8531 <reader2027@gmail.com>
2024-05-13 06:51:35 +0200

    remove: unnecessary includes

--- ft_ping.c
+++ ft_ping.c
@@ -4,11 +4,9 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <math.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <unistd.h>
 
 extern t_ft_ping ping;
